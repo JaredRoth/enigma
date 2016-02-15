b7721e005a2f44a73242778db1ecb661aafdd475
Jared Roth <jared.roth@gmail.com>
2016-02-15 13:23:54 -0700

    Test file

---
+++ enigma_test.rb
@@ -0,0 +1,16 @@
+require "minitest"
+require_relative 'enigma'
+
+class EnigmaTest < Minitest::Test
+  def test_add_date_offsets
+    
+  end
+
+  def test_build_map
+
+  end
+
+  def test_build_rotation
+
+  end
+end
