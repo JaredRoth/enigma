5ce6f22ad7a99232fb589d430b50b4f25fba8793
Jared Roth <jared.roth@gmail.com>
2016-02-16 08:30:27 -0700

    Monday morning changes

--- enigma.rb
+++ enigma.rb
@@ -1,26 +1,28 @@
 class Enigma
-  def encrypt(message, key = (1..5).map{rand(9)}, date = Time.now)
-    cypher = build_cypher
+  def encrypt(message, key = nil, date = Time.now)
+    key_map = build_key_map
     rotation_index = 0
+    key.nil? ? key = (1..5).map{rand(9)} : key = key.chars
     date = date.strftime("%d%m%y").to_i
     rotation = add_date_offsets(build_rotation(key), date)
+
     message.chars.map do |char|
       rotation_index = 0 if rotation_index == rotation.length
-      new_char = cypher[(cypher.index(char) + rotation[rotation_index]) % 39]
+      new_char = key_map[(key_map.index(char) + rotation[rotation_index]) % 39]
       rotation_index += 1
       new_char
     end.join("")
   end
 
-  def add_date_offsets(rotation, date)
-    offsets = (date*date).to_s[-(rotation.length)..-1]
-    0.upto(rotation.length - 1) do |i|
-      rotation[i] += offsets[i].to_i
+  def add_date_offsets(abcd, date)
+    offsets = (date*date).to_s[-(abcd.length)..-1]
+    0.upto(abcd.length - 1) do |i|
+      abcd[i] += offsets[i].to_i
     end
-    rotation
+    abcd
   end
 
-  def build_cypher
+  def build_key_map
     local_map = ("a".."z").to_a
     ("0".."9").to_a.each do |i|
       local_map << i
@@ -34,5 +36,3 @@ class Enigma
     end
   end
 end
-e = Enigma.new
-p e.encrypt("words", [1,2,3,4,5], Time.now)
